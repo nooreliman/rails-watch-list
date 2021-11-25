3a38808e888c35441e93c6e351642c4139c38458
iman fuad <nooreliman@gmail.com>
2021-11-25 17:50:51 +0800

    added remaining code to complete all functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "Starting to seed movies"
+Movie.destroy_all
+
+url = 'https://tmdb.lewagon.com/movie/top_rated?api_key=%3Cyour_api_key%3E'
+response = URI.open(url).read
+movie_array = JSON.parse(response)["results"].first(10)
+movie_array.each do |movie|
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w300" + movie["poster_path"],
+    rating: movie["vote_average"]
+  )
+end
+puts "Done seeding #{Movie.count} movies!"
+
+
+
