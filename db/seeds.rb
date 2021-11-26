609ac5fb51264377cf9b40db96ae1eb9d8abca25
iman fuad <nooreliman@gmail.com>
2021-11-26 18:02:13 +0800

    made nicer

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,9 +23,3 @@ movie_array.each do |movie|
   )
 end
 puts "Done seeding #{Movie.count} movies!"
-
-require "open-uri"
-
-file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg')
-article = Article.new(title: 'NES', body: "A great console")
-article.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
